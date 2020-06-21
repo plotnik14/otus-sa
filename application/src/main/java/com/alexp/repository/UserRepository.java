810e7b855e6a68983fe7d2122ef4331f324aff88
plotnik14 <plotnikov93av@gmail.com>
2020-06-21 21:37:27 +0300

    added app

---
+++ application/src/main/java/com/alexp/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.alexp.repository;
+
+import com.alexp.model.User;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRepository extends CrudRepository<User, Long> {
+}
