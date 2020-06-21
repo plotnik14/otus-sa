2a4919d8bca1cbc9f00c87b4a1aff0f0c6574da2
plotnik14 <plotnikov93av@gmail.com>
2020-06-21 21:44:04 +0300

    added docker HW

---
+++ 1. Docker/src/main/java/com/alexp/OtusApplication.java
@@ -0,0 +1,11 @@
+package com.alexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtusApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OtusApplication.class, args);
+    }
+}
