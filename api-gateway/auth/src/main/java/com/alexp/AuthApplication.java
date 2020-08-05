f04356b0afe58233753dd9bee23f2f8b91444e9d
plotnik14 <plotnikov93av@gmail.com>
2020-08-05 23:04:31 +0300

    Init Auth

---
+++ api-gateway/auth/src/main/java/com/alexp/AuthApplication.java
@@ -0,0 +1,10 @@
+package com.alexp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+public class AuthApplication {
+        public static void main(String[] args) {
+            SpringApplication.run(AuthApplication.class, args);
+        }
+}
