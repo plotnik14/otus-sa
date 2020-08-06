05ba16921ab0046a77b1640ab19a79396dedc3e2
plotnik14 <plotnikov93av@gmail.com>
2020-08-06 14:55:35 +0300

    Init User Management

---
+++ api-gateway/user-management/src/main/java/com/alexp/controller/UserManagementController.java
@@ -0,0 +1,13 @@
+package com.alexp.controller;
+
+import com.alexp.model.Response;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class UserManagementController {
+    @GetMapping("/health")
+    public Response healthCheck() {
+        return new Response("OK");
+    }
+}
