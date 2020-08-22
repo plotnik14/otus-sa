9df8290a54c277442c3f515ef67e016f394498e2
plotnik14 <plotnikov93av@gmail.com>
2020-08-22 14:25:23 +0300

    PostgreSPL for UM

--- project/sources/user-management/src/main/java/com/alexp/model/User.java
+++ project/sources/user-management/src/main/java/com/alexp/model/User.java
@@ -2,9 +2,11 @@ package com.alexp.model;
 
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import java.util.UUID;
 
 @Entity
+@Table(name = "users")
 public class User {
     @Id
     private UUID userId;
