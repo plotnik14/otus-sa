e829e51062866e492b9a6e7bed81129d6d44019a
plotnik14 <plotnikov93av@gmail.com>
2020-08-23 16:19:01 +0300

    A&A for UM

--- project/sources/user-management/src/main/java/com/alexp/controller/UserManagementController.java
+++ project/sources/user-management/src/main/java/com/alexp/controller/UserManagementController.java
@@ -4,15 +4,12 @@ import com.alexp.model.User;
 import com.alexp.repositoriy.UserRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.http.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import java.util.UUID;
 
 @RestController
@@ -79,39 +76,13 @@ public class UserManagementController {
     }
 
     private boolean authorizedUser(HttpServletRequest httpServletRequest, String userId) {
-        if (true) { // ToDO DELETE!!!!!!!!!!!!!!!!!!!
-            return true;
-        }
+        String userIdFromHeader = httpServletRequest.getHeader("X-UserId");
+        String role = httpServletRequest.getHeader("X-Role");
 
-        if (httpServletRequest.getCookies() == null){
-            LOGGER.debug("cookies are null");
-            return false;
-        }
+        LOGGER.debug("userIdFromHeader:{}", userIdFromHeader);
+        LOGGER.debug("role:{}", role);
 
-        List<Cookie> cookies = new ArrayList<Cookie>(Arrays.asList(httpServletRequest.getCookies()));
-        Cookie sessionIdCookie = cookies.stream()
-                .filter(cookie -> "sessionId".equals(cookie.getName()))
-                .findFirst()
-                .orElse(null);
-
-        if (sessionIdCookie == null){
-            LOGGER.debug("sessionId hasn't been found in cookies:{}", cookies);
-            return false;
-        }
-
-        HttpHeaders httpHeaders = new HttpHeaders();
-        httpHeaders.add(HttpHeaders.COOKIE, "sessionId="+sessionIdCookie.getValue());
-
-        LOGGER.debug("before rest to auth");
-
-        // ToDo DO NOT SEND REQUEST. You already have all info
-        ResponseEntity<Object> responseEntity = restTemplate.exchange("http://auth:9000/auth", HttpMethod.GET, new HttpEntity<>(httpHeaders), Object.class);
-
-        LOGGER.debug("after rest to auth");
-
-        String userIdHeader = responseEntity.getHeaders().get("X-UserId").get(0);
-
-        return userIdHeader.equals(userId);
+        return userId.equals(userIdFromHeader) || role.equals("Admin");
     }
 
 }
